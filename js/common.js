edc79cb09f5919f80efa4d2c0543ca9632daa54a
조현창 <emrdl7@gmail.com>
2018-01-06 14:07:38 +0900

    코딩 1차 완료

--- js/common.js
+++ js/common.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
-  $("header nav").hover(function() {
-    $(this).addClass("active");
-  }, function() {
-    $(this).removeClass("active");
+  $(".screen button.cave").on("click", function() {
+    var switchon = $(this).attr("data");
+    $(".screen button.cave").removeClass("active");
+    $(".screen button."+ switchon).addClass("active");
+
+    $(".screen .cavedata").removeClass("active");
+    $(".screen .cavedata."+ switchon).addClass("active");
   });
 
+  $(".screen ul button").on("click", function() {
+    var tabon = $(this).attr("class");
+    $(".screen ul button").removeClass("active");
+    $(".screen ul button."+ tabon).addClass("active");
+
+    $(".screen .sortdata").removeClass("active");
+    $(".screen .sortdata."+ tabon).addClass("active");
+  });
 
 
 });
